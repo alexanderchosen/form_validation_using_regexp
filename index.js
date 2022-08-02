b2f31aa43bcaecbcea1ab65dc0e0b1a6fdf4c932
Alexander Chosen okon <chosenokonalexander@gmail.com>
2022-08-02 00:11:08 +0000

    This is my 3rd commit

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ let fullName = document.getElementById('full-name');
 let username = document.getElementById('user-name');
 let password = document.getElementById('password');
 let phoneNumber = document.getElementById('phone-number');
+let email = document.getElementById('email');
+let favWebsite = document.getElementById('fav-website')
  
-let fullNameError = document.getElementsByClassName('input-name-error')
+let fullNameError = document.getElementsByClassName('name-error')
 let usernameError = document.getElementsByClassName('user-name-error')
 let passwordError = document.getElementsByClassName('password-error')
 let numberError = document.getElementsByClassName('number-error')
+let emailError = document.getElementsByClassName('email-error')
+let websiteError = document.getElementsByClassName('website-error')
 
-username.addEventListener('input', function(){
-    let pattern = username.getAttribute('pattern')
-    let currentValue = e.target.value
-    let success = pattern.test(currentValue)
-    if(success){
+
+username.addEventListener('input', function(e){
+    let usernamePattern = username.getAttribute('pattern')
+    let usernameValue = e.target.value
+    let usernameSuccess = usernamePattern.test(usernameValue)
+    if(usernameSuccess){
         usernameError.style.display = 'none'
     }
     else{
@@ -20,4 +25,18 @@ username.addEventListener('input', function(){
     }
 })
 
-// write a general function that can be called for every eventListener and then firdt retrieve the element it is called in which is later passed into the pattern variable to get its specific pattern attribute
+// write a general function that can be called for every eventListener and then first retrieve the element it is called 
+// in which is later passed into the pattern variable to get its specific pattern attribute
+
+
+favWebsite.addEventListener('input', function(e){
+    let webPattern = new RegExp ("(http(s?)://)(www)\.[\w_+-]+\.(\w){2,}$" , "gi")
+    let websiteValue = e.target.value
+    let webSuccess = webPattern.test(websiteValue)
+    if (webSuccess){
+        websiteError.style.display='none'
+    }
+    else{
+        websiteError.style.display ='block'
+    }
+})
